a852082abcf4314e359be8412b9b594467119820
Vitaliy <theyamshikov@yandex.ru>
2024-03-17 14:27:25 +0500

    upd

--- src/resources/config/services/main.php
+++ src/resources/config/services/main.php
@@ -10,7 +10,7 @@ use Untek\FrameworkPlugin\RestApiOpenApiGenerator\Domain\Subscribers\GenerateOpe
 use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 return static function (ContainerConfigurator $configurator): void {
-    $services = $configurator->services();
+    $services = $configurator->services()->defaults()->public();
 
     $services->set(OpenApi3::class, OpenApi3::class)
         ->args([
