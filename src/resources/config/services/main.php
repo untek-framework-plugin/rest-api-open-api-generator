0ba0fd8d9b4fdf4cd9b20ff30d215cc0ce345992
Vitaliy <theyamshikov@yandex.ru>
2024-08-10 16:09:18 +0500

    upd

--- src/resources/config/services/main.php
+++ src/resources/config/services/main.php
@@ -1,10 +1,6 @@
 <?php
 
-use Psr\Container\ContainerInterface;
-use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
-use Untek\Framework\RestApi\Presentation\Http\Symfony\Subscribers\RestApiHandleSubscriber;
-use Untek\FrameworkPlugin\RestApiErrorHandle\Presentation\Http\Symfony\Controllers\RestApiErrorController;
 use Untek\FrameworkPlugin\RestApiOpenApiGenerator\Domain\Libs\OpenApi3\OpenApi3;
 use Untek\FrameworkPlugin\RestApiOpenApiGenerator\Domain\Subscribers\GenerateOpenApiDocsSubscriber;
 use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
@@ -17,7 +13,7 @@ return static function (ContainerConfigurator $configurator): void {
             getenv('OPEN_API_REST_API_SOURCE_DIRECTORY')
         ]);
 
-    if(getenv('OPEN_API_ENABLED')) {
+    if (getenv('OPEN_API_ENABLED')) {
         $services->set(GenerateOpenApiDocsSubscriber::class, GenerateOpenApiDocsSubscriber::class)
             ->args([
                 service(OpenApi3::class)
